a4b8961dbf616704b2e60ffbacd7304e6bd85038
Mike-E <michael@dragonspark.network>
2020-05-04 12:58:26 -0400

    Added additional tests for a total of 100.

---
+++ EFCore.Performance.xUnit/UnitTest17.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EFCore.Performance.xUnit
+{
+	public sealed class UnitTest17
+
+	{
+		[Fact]
+		public Task Test1() => Run.Default.Execute();
+
+		[Fact]
+		public Task Test2() => Run.Default.Execute();
+
+		[Fact]
+		public Task Test3() => Run.Default.Execute();
+
+		[Fact]
+		public Task Test4() => Run.Default.Execute();
+
+		[Fact]
+		public Task Test5() => Run.Default.Execute();
+	}
+}
