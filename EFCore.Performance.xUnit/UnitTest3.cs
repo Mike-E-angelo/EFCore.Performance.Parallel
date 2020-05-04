33063392384715ac7b1172809cce78a18234a86d
Mike-E <michael@dragonspark.network>
2020-05-04 12:35:56 -0400

    Added more tests.

---
+++ EFCore.Performance.xUnit/UnitTest3.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EFCore.Performance.xUnit
+{
+	public sealed class UnitTest3
+	{
+		[Fact]
+		public Task Test1() => Run.Default.Execute();
+
+		[Fact]
+		public Task Test2() => Run.Default.Execute();
+
+		[Fact]
+		public Task Test3() => Run.Default.Execute();
+
+		[Fact]
+		public Task Test4() => Run.Default.Execute();
+
+		[Fact]
+		public Task Test5() => Run.Default.Execute();
+	}
+}
